8eb88927dd4d45dca2e9a11982790e6e818ed84a
xorps <758027+xorps@users.noreply.github.com>
2021-07-16 03:58:57 -0700

    cleanup

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,5 +1,6 @@
 const { Op } = require("sequelize");
 const db = require("../db");
+const Message = require("./message");
 const Sequelize = require("sequelize");
 
 const Conversation = db.define("conversation", {
